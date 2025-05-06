51055ea3b1e8329be6a5fe658710a4828c821131
ssssota <tommysouta@gmail.com>
2025-05-06 19:00:46 +0900

    chore(playground): fix options ui

--- playground/src/app.tsx
+++ playground/src/app.tsx
@@ -67,8 +67,8 @@ export function App() {
 					}}
 					style={{ fontFamily: "monospace" }}
 				/>
-				<fieldset>
-					<legend>Options</legend>
+				<details>
+					<summary>Options</summary>
 					<p>
 						<label for="hlsl">
 							<input
@@ -82,11 +82,14 @@ export function App() {
 									};
 								}}
 							/>
-							HLSL
+							HLSL (default is GLSL)
 						</label>
 					</p>
 					<p>
-						<label for="format">Format</label>
+						<label for="format">
+							Format (the output format (use 'text' if you want just the
+							shader))
+						</label>
 						<select
 							id="format"
 							value={options.value.format}
@@ -106,11 +109,35 @@ export function App() {
 							<option value="rust">rust</option>
 						</select>
 					</p>
-				</fieldset>
-				<fieldset>
-					<legend>Field names</legend>
 					<p>
-						<label for="field-names">Field names</label>
+						<label for="no-renaming-list">
+							No renaming list (Comma-separated list of functions to preserve)
+							<input
+								type="text"
+								id="no-renaming-list"
+								placeholder="foo, bar"
+								value={
+									Array.isArray(options.value.noRenamingList)
+										? options.value.noRenamingList.join(", ")
+										: ""
+								}
+								onInput={(e) => {
+									options.value = {
+										...options.value,
+										noRenamingList: e.currentTarget.value
+											.split(",")
+											.map((s) => s.trim())
+											.filter((s) => s.length > 0),
+									};
+								}}
+							/>
+						</label>
+					</p>
+					<p>
+						<label for="field-names">
+							Field names (the field names for vectors: 'rgba', 'xyzw', or
+							'stpq')
+						</label>
 						<select
 							id="field-names"
 							value={options.value.fieldNames}
@@ -126,175 +153,153 @@ export function App() {
 							<option value="stpq">stpq</option>
 						</select>
 					</p>
-				</fieldset>
-				<p>
-					<label for="preserve-externals">
-						<input
-							type="checkbox"
-							id="preserve-externals"
-							checked={options.value.preserveExternals}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									preserveExternals: e.currentTarget.checked,
-								};
-							}}
-						/>
-						Preserve externals
-					</label>
-				</p>
-				<p>
-					<label for="preserve-all-globals">
-						<input
-							type="checkbox"
-							id="preserve-all-globals"
-							checked={options.value.preserveAllGlobals}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									preserveAllGlobals: e.currentTarget.checked,
-								};
-							}}
-						/>
-						Preserve all globals
-					</label>
-				</p>
-				<p>
-					<label for="no-inlining">
-						<input
-							type="checkbox"
-							id="no-inlining"
-							checked={options.value.noInlining}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									noInlining: e.currentTarget.checked,
-								};
-							}}
-						/>
-						No inlining
-					</label>
-				</p>
-				<p>
-					<label for="aggressive-inlining">
-						<input
-							type="checkbox"
-							id="aggressive-inlining"
-							checked={options.value.aggressiveInlining}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									aggressiveInlining: e.currentTarget.checked,
-								};
-							}}
-						/>
-						Aggressive inlining
-					</label>
-				</p>
-				<p>
-					<label for="no-renaming">
-						<input
-							type="checkbox"
-							id="no-renaming"
-							checked={options.value.noRenaming}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									noRenaming: e.currentTarget.checked,
-								};
-							}}
-						/>
-						No renaming
-					</label>
-				</p>
-				<p>
-					<label for="no-renaming-list">
-						<input
-							type="text"
-							id="no-renaming-list"
-							placeholder="foo, bar"
-							value={
-								Array.isArray(options.value.noRenamingList)
-									? options.value.noRenamingList.join(", ")
-									: ""
-							}
-							onInput={(e) => {
-								options.value = {
-									...options.value,
-									noRenamingList: e.currentTarget.value
-										.split(",")
-										.map((s) => s.trim())
-										.filter((s) => s.length > 0),
-								};
-							}}
-						/>
-						No renaming list
-					</label>
-				</p>
-				<p>
-					<label for="no-sequence">
-						<input
-							type="checkbox"
-							id="no-sequence"
-							checked={options.value.noSequence}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									noSequence: e.currentTarget.checked,
-								};
-							}}
-						/>
-						No sequence
-					</label>
-				</p>
-				<p>
-					<label for="no-remove-unused">
-						<input
-							type="checkbox"
-							id="no-remove-unused"
-							checked={options.value.noRemoveUnused}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									noRemoveUnused: e.currentTarget.checked,
-								};
-							}}
-						/>
-						No remove unused
-					</label>
-				</p>
-				<p>
-					<label for="no-overloading">
-						<input
-							type="checkbox"
-							id="no-overloading"
-							checked={options.value.noOverloading}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									noOverloading: e.currentTarget.checked,
-								};
-							}}
-						/>
-						No overloading
-					</label>
-				</p>
-				<p>
-					<label for="move-declarations">
-						<input
-							type="checkbox"
-							id="move-declarations"
-							checked={options.value.moveDeclarations}
-							onChange={(e) => {
-								options.value = {
-									...options.value,
-									moveDeclarations: e.currentTarget.checked,
-								};
-							}}
-						/>
-						Move declarations
-					</label>
-				</p>
+					<p>
+						<label for="preserve-externals">
+							<input
+								type="checkbox"
+								id="preserve-externals"
+								checked={options.value.preserveExternals}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										preserveExternals: e.currentTarget.checked,
+									};
+								}}
+							/>
+							Preserve externals (Do not rename external values (e.g. uniform))
+						</label>
+					</p>
+					<p>
+						<label for="preserve-all-globals">
+							<input
+								type="checkbox"
+								id="preserve-all-globals"
+								checked={options.value.preserveAllGlobals}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										preserveAllGlobals: e.currentTarget.checked,
+									};
+								}}
+							/>
+							Preserve all globals (Do not rename functions and global
+							variables)
+						</label>
+					</p>
+					<p>
+						<label for="no-inlining">
+							<input
+								type="checkbox"
+								id="no-inlining"
+								checked={options.value.noInlining}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										noInlining: e.currentTarget.checked,
+									};
+								}}
+							/>
+							No inlining (Do not automatically inline variables and functions)
+						</label>
+					</p>
+					<p>
+						<label for="aggressive-inlining">
+							<input
+								type="checkbox"
+								id="aggressive-inlining"
+								checked={options.value.aggressiveInlining}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										aggressiveInlining: e.currentTarget.checked,
+									};
+								}}
+							/>
+							Aggressive inlining
+						</label>
+					</p>
+					<p>
+						<label for="no-renaming">
+							<input
+								type="checkbox"
+								id="no-renaming"
+								checked={options.value.noRenaming}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										noRenaming: e.currentTarget.checked,
+									};
+								}}
+							/>
+							No renaming
+						</label>
+					</p>
+					<p>
+						<label for="no-sequence">
+							<input
+								type="checkbox"
+								id="no-sequence"
+								checked={options.value.noSequence}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										noSequence: e.currentTarget.checked,
+									};
+								}}
+							/>
+							No sequence (Do not use the comma operator trick)
+						</label>
+					</p>
+					<p>
+						<label for="no-remove-unused">
+							<input
+								type="checkbox"
+								id="no-remove-unused"
+								checked={options.value.noRemoveUnused}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										noRemoveUnused: e.currentTarget.checked,
+									};
+								}}
+							/>
+							No remove unused
+						</label>
+					</p>
+					<p>
+						<label for="no-overloading">
+							<input
+								type="checkbox"
+								id="no-overloading"
+								checked={options.value.noOverloading}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										noOverloading: e.currentTarget.checked,
+									};
+								}}
+							/>
+							No overloading (When renaming functions, do not introduce new
+							overloads)
+						</label>
+					</p>
+					<p>
+						<label for="move-declarations">
+							<input
+								type="checkbox"
+								id="move-declarations"
+								checked={options.value.moveDeclarations}
+								onChange={(e) => {
+									options.value = {
+										...options.value,
+										moveDeclarations: e.currentTarget.checked,
+									};
+								}}
+							/>
+							Move declarations
+						</label>
+					</p>
+				</details>
 			</form>
 			<output>
 				{duration.value && <p>Minified in {duration.value.toFixed(2)}ms</p>}
